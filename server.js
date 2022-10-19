db725cb1555cda9a43e6b819997a0d22d367e0e6
Esra Al <esra.wameed@yahoo.com>
2022-10-19 19:20:49 -0400

    added db connection and placed password in .env

--- server.js
+++ server.js
@@ -4,3 +4,27 @@ const inquier = require('inquirer');
 const cTable = require('console.table');
 const figlet = require('figlet');
 require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password:Pass_DB,
+  database:'employeeTrack_DB',
+});
+
+
+
+
+
+
+//listening on port 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
