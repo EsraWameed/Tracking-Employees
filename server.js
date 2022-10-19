7fb6be89a544086d579aa2e6ab8bb53dd8d2a724
Esra Al <esra.wameed@yahoo.com>
2022-10-19 19:12:48 -0400

    installed and required dependencies and included node_modules in .gitinore

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquier = require('inquirer');
+const cTable = require('console.table');
+const figlet = require('figlet');
+require('dotenv').config();
