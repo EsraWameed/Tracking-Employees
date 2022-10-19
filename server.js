e15a7f34b53a26648a8d7dceb92efd26300844c4
Esra Al <esra.wameed@yahoo.com>
2022-10-19 19:34:32 -0400

    created connection to db and fixed .env

--- server.js
+++ server.js
@@ -4,3 +4,28 @@ const inquier = require('inquirer');
 const cTable = require('console.table');
 const figlet = require('figlet');
 require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: Pass_DB,
+  database: 'employeeTrack_DB',
+});
+
+
+
+
+
+
+//Listening to port
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
