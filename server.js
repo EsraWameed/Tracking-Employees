13caeff479d78dd6cd51a0bd9299d5e6cb9c9e17
Esra Al <esra.wameed@yahoo.com>
2022-10-19 22:03:00 -0400

    added function to add employee

--- server.js
+++ server.js
@@ -164,7 +164,74 @@ const departmentAdd = () => {
         });
       });
     }
-
+// function to add a new employee
+const employeeAdd = () => {
+  connection.query("SELECT * FROM EMPLOYEE", (err, resEmployee) => {
+    if (err) throw err;
+    const employeeSelection = [
+      {
+        name: 'None',
+        value: 0
+      }
+    ]; 
+    resEmployee.forEach(({ first_name, last_name, id }) => {
+      employeeSelection.push({
+        name: first_name + " " + last_name,
+        value: id
+      });
+    });
+    
+    connection.query("SELECT * FROM ROLE", (err, rolRes) => {
+      if (err) throw err;
+      const roleChoice = [];
+      rolRes.forEach(({ title, id }) => {
+        roleChoice.push({
+          name: title,
+          value: id
+          });
+        });
+     
+      let questionsArray = [
+        {
+          type: "input",
+          name: "first_name",
+          message: "employee's first name?"
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "employee's last name?"
+        },
+        {
+          type: "list",
+          name: "role_id",
+          choices: roleChoice,
+          message: "what is the employee's role?"
+        },
+        {
+          type: "list",
+          name: "manager_id",
+          choices: employeeSelection,
+          message: "Who is this employee's manager if any exist?"
+        }
+      ]
+  
+      inquier.prompt(questionsArray)
+        .then(response => {
+          const query = `INSERT INTO EMPLOYEE (first_name, last_name, role_id, manager_id) VALUES (?)`;
+          let manager_id = response.manager_id !== 0? response.manager_id: null;
+          connection.query(query, [[response.first_name, response.last_name, response.role_id, manager_id]], (err, res) => {
+            if (err) throw err;
+            console.log(`successfully inserted employee`);
+            choiceSelection();
+          });
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    })
+  });
+}
 
 
 
